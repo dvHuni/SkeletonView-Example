ec6291973bf812277309ad311e50e4e7f815338e
AhnSangHoon <ansh0324@gmail.com>
2021-08-23 10:00:21 +0900

    INIT] Initialize Project

---
+++ SkeletonViewExample/SkeletonViewExample/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  SkeletonViewExample
+//
+//  Created by AhnSangHoon on 2021/08/21.
+//
+
+import UIKit
+import SkeletonView
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ SkeletonViewExample/SkeletonViewExample/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  SkeletonViewExample
+//
+//  Created by AhnSangHoon on 2021/08/21.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+}
+
